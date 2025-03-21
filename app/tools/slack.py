e18a51abf609dd22d16f0f0e0ac24287c87b3269
Gaurav R <gaurav.r@innovalabs.tech>
2025-03-21 08:27:45 +0530

    slack message with proxy

--- app/tools/slack.py
+++ app/tools/slack.py
@@ -1,6 +1,7 @@
 
 from fastapi import APIRouter, HTTPException, Query, Path
 from app.config import PIPEDREAM_API_HOST, OAUTH_TOKEN, PIPEDREAM_PROJECT_ID, PIPEDREAM_PROJECT_ENVIRONMENT, CLIENT_ID, CLIENT_SECRET, BASE_URL
+from app.helpers import encode_url
 import requests
 
 routes = APIRouter(tags=["Slack"])
@@ -70,6 +71,39 @@ def send_message(
         "X-PD-Environment": "development"
     }
     response = requests.post(url, json=payload, headers=headers)
+    if response.status_code != 200:
+        raise HTTPException(status_code=response.status_code, detail=response.text)
+    return response.json()
+@routes.post("/connect/{project_id}/components/{action_name}/run/slack/send_message/proxy")
+def send_message_proxy(
+        project_id: str,
+        external_user_id: str = Query(..., description="External user ID, e.g. abc-123",placeholder="31b294c4-450f-446c-ad4d-c49178f577de"),
+        apn_key: str = Query(..., description="Connected Slack account ID, e.g. apn_gyhGpEj",placeholder="apn_gyhGpEj")
+):
+    """
+    Execute a specific action for a user.
+    """
+    slack_url = "https://slack.com/api/chat.postMessage"
+    encoded_url = encode_url(slack_url)
+    url = f"{BASE_URL}/connect/{project_id}/proxy/{encoded_url}"
+    
+    payload = {
+        "text": "hi",
+        "channel": "C0772SYKNN4"
+    }
+    
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {OAUTH_TOKEN}",
+        "X-PD-Environment": "development"
+    }
+    
+    params = {
+        "external_user_id": external_user_id,
+        "account_id": apn_key
+    }
+    
+    response = requests.post(url, json=payload, headers=headers, params=params)
     if response.status_code != 200:
         raise HTTPException(status_code=response.status_code, detail=response.text)
     return response.json()
